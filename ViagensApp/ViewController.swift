adf2d5f49683cf650ff21644c95f7739d89783cf
Caio Mori <caiomori22@gmail.com>
2024-11-23 23:46:03 -0300

    feat: initial commit

--- ViagensApp/ViewController.swift
+++ ViagensApp/ViewController.swift
@@ -7,11 +7,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    @IBOutlet weak var tripsTableOutlet: UITableView!
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        
+        cell.textLabel?.text = "Viagem \(indexPath.row)"
+        
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tripsTableOutlet.dataSource = self
     }
 
 
