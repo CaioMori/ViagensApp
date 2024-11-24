148c246beda19e4d1f12da31e6acbcbf81e1d687
Caio Mori <caiomori22@gmail.com>
2024-11-24 00:29:46 -0300

    feat: added HomeTableViewHeader

---
+++ ViagensApp/HomeTableViewHeader.swift
@@ -0,0 +1,20 @@
+//
+//  HomeTableViewHeader.swift
+//  ViagensApp
+//
+//  Created by Caio Mori on 24/11/24.
+//
+
+import UIKit
+
+class HomeTableViewHeader: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
